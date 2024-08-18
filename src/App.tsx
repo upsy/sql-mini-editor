6a53900c505d0e0abc3e239d918322ae9e57c59e
upsy <newbieag@gmail.com>
2024-08-18 11:24:59 +0300

    fullscreen bg

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function SQLDashboard({ bpmEngine, url }: { bpmEngine: BPMEngine, url: string })
   return (
     <DashboardContext.Provider value={{ bpmEngine: bpmEngine, url: url }}>
       <QueryClientProvider client={queryClient}>
-        <div className="px-4 py-2 bg-zinc-50">
+        <div className="px-4 py-2 bg-zinc-50 min-h-screen">
           <FullDashboard />
         </div>
         <Toaster></Toaster>
@@ -88,7 +88,7 @@ function FullDashboard() {
 
   });
 
-  if (!fetchedSchema) return (<FullDashboardSkeleton/>);
+  if (!fetchedSchema) return (<FullDashboardSkeleton />);
   if (!allowedTables) return (<p>Error.. failed to load allowed tables from server!</p>);
 
   if (error) return (<p>{'Error loading schema ' + error.toString()}</p>);
@@ -122,8 +122,8 @@ function FullDashboardSkeleton() {
       <Skeleton className="h-[40px] w-[400px] rounded-md m-2 bg-zinc-200" />
       <div className="flex p-2 w-full flex-col gap-2">
         <div className="flex flex-row gap-2 w-full">
-          <Skeleton className="w-2/3 h-[380px] bg-zinc-200"/>
-          <Skeleton className="w-1/3 h-[380px] bg-zinc-200"/>
+          <Skeleton className="w-2/3 h-[380px] bg-zinc-200" />
+          <Skeleton className="w-1/3 h-[380px] bg-zinc-200" />
         </div>
         <Skeleton className="w-full h-[146px] bg-zinc-200" />
       </div>
